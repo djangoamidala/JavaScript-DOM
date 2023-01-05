fe39a2bc7d06748d75021e6d785b5bd6ebf97cce
Andy Dabek <andy.dabzouk@gmail.com>
2023-01-05 21:17:22 +0100

    Update

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 //   1. Affichez une alerte avec le message de votre choix lorsque l'utilisateur clique sur un bouton
 const monBouton = document.getElementById('monBouton');
+const paragraph = document.getElementById('monParagraphe');
 // Affiche une alert quand on click sur le bouton
 monBouton.addEventListener('click', function () {
-    alert("Attention")
+    // alert("Attention") (Désactiver)
+    // On modifie le text du paragraphe
+    paragraph.textContent = 'Je suis un paragraphe modifié'
 })
+// 2. Modifiez le style du paragraphe lorsque l'utilisateur passe la souris dessus (la couleur et le font size)
 
-// 2.Modifier le texte du paragraphe par : 'Je suis un paragraphe modifié' à la place de l'alerte (commentez la ligne avec l'alerte)
-const paragraph = document.getElementById('monParagraphe');
-// On modifie le text du paragraphe
-paragraph.textContent = 'Je suis un paragraphe modifié'
-
-
-// 3. Modifiez le style du paragraphe lorsque l'utilisateur passe la souris dessus (la couleur et le font size)
 // On modifie les propriétés quand le curseur de souris entre dans la zone du contenu
 paragraph.addEventListener("mouseenter", function (event) {
     event.target.style.color = "orange";
@@ -23,7 +20,8 @@ paragraph.addEventListener('mouseleave', function (event) {
     event.target.style.fontSize = "";
 }
 )
-// 4. Ajoutez (créer) un nouvel élément HTML à la page lorsque l'utilisateur clique sur un bouton et l'ajouter en tant qu'element enfant de la liste et lui appliqué le texte : 'Nouvel élément'
+// 3. Ajoutez (créer) un nouvel élément HTML à la page lorsque l'utilisateur clique sur un bouton et l'ajouter en tant qu'element enfant de la liste et lui appliqué le texte : 'Nouvel élément'
+
 // On attribu un nouvel element 'li' qui a pour texte : 'Nouvel élément', dans l'element qui porte l'id maListe
 monBouton.addEventListener('click', function () {
     let item = document.createElement('li');
